2e166bb6d3e524ec590b66d040bf62d49dd547b1
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2020-09-05 23:25:57 -0300

    Add login template
    With random backgrounds of Argentina

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
+import LoginTemplateSideBar from "components/Login/LoginTemplateSideBar";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import LoginButton from "components/LoginButton";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Log In</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/login">
-            <LoginButton></LoginButton>
-          </Route>
-          <Route path="/"></Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/login">
+          <LoginTemplateSideBar></LoginTemplateSideBar>
+        </Route>
+        <Route path="/"></Route>
+      </Switch>
     </Router>
   );
 }
