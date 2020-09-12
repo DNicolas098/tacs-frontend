a516bbafd33f2296652f41e8e9f5b301cd22c32b
Pablo Paglilla <pabpagli@publicisgroupe.net>
2020-09-12 04:36:13 -0300

    Implementar flujo de signUp/logIn/logOut

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import LoginTemplateSideBar from "components/Login/LoginTemplateSideBar";
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "components/Dashboard/Dashboard";
+import { BaseWololoApiClient } from "api/client";
 
 export default function App() {
+
+  const baseApiClient = new BaseWololoApiClient();
+  const [isLoggedIn, setLoggedIn] = React.useState(baseApiClient.userIsLoggedIn());
+
+  const setUserLoggedIn = () => setLoggedIn(true);
+  const setUserLoggedOut = () => setLoggedIn(false);
+
   return (
     <Router>
       <Switch>
         <Route path="/login">
-          <LoginTemplateSideBar></LoginTemplateSideBar>
+          <LoginTemplateSideBar
+            flagLoggedIn={setUserLoggedIn} />
         </Route>
         <Route path="/app">
-          <Dashboard></Dashboard>
+          <Dashboard
+            flagLoggedOut={setUserLoggedOut} />
         </Route>
-        <Route path="/"></Route>
       </Switch>
+      {isLoggedIn ? <Redirect to="/app" /> : <Redirect to="/login" />}
     </Router>
   );
 }
